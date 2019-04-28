3945987c4ae96bdcf5e9fe807ab4497477acad27
Mandana <mandanakhademi88@gmail.com>
2019-04-28 11:28:30 +0100

    increase count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,20 @@ class Counter extends Component {
     count: 0
   };
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
