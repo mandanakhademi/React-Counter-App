a5cb6865fbf723a669ef7e742c921516e3addd09
Mandana <mandanakhademi88@gmail.com>
2019-05-13 15:15:55 +0100

    update button labels

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,20 +14,20 @@ class Counter extends Component {
             onClick={() => this.props.onIncrement(this.props.counter)}
             className="btn btn-secondary btn-sm"
           >
-            Increment
+            +
           </button>
           <button
             onClick={() => this.props.onDecrement(this.props.counter)}
             className="btn btn-secondary btn-sm m-2"
             disabled={this.props.counter.value === 0 ? 'disabled' : ''}
           >
-            Decrement
+            -
           </button>
           <button
             onClick={() => this.props.onDelete(this.props.counter.id)}
             className="btn btn-danger btn-sm"
           >
-            Delete
+            x
           </button>
         </div>
 
