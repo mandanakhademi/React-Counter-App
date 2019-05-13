062d6903623eed08be1aa26a0f9fd3b7d2d3b7ae
Mandana <mandanakhademi88@gmail.com>
2019-05-13 15:11:45 +0100

    add decrement functionality

--- src/App.js
+++ src/App.js
@@ -39,6 +39,18 @@ class App extends Component {
     });
   }
 
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {
+      ...counter
+    };
+    counters[index].value--;
+    this.setState({
+      counters
+    });
+  }
+
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({
@@ -65,6 +77,8 @@ class App extends Component {
             onReset={this.handleReset}
             onDelete={this.handleDelete}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+
           />
         </main>   
       </React.Fragment>
